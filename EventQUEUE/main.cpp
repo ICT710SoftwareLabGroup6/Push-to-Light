14423220cef8fad8c8f577e2fb7198ca46053b36
unknown <pwnp09@gmail.com>
2020-02-25 12:55:57 +0700

    edit main

--- EventQUEUE/main.cpp
+++ EventQUEUE/main.cpp
@@ -1,23 +1,32 @@
 #include "mbed.h"
 
-DigitalOut led(LED1);
-InterruptIn btn(PC_13);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+InterruptIn sw(PC_13);
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
 Thread t;
 
 void toggle(void) {
  
-   
-    led = !led;
+    led2 = !led2;
+
 }
-
-
-
 int main() {
-    led = 0;
-    t.start(callback(&queue, &EventQueue::dispatch_forever));
-    
-    btn.rise(toggle);
+	led2 = 0;          //initial value, set led2 turn off.
+	
+	t.start(callback(&queue, &EventQueue::dispatch_forever));
+    sw.rise(toggle);                           // when you pushed blue button, the led2 turn on. 
+    sw.fall(queue.event(toggle));            // when you released blue botton, the led2 turn off. 
+	
+	
+	   //led1 blink.
+	while(1) {                    
+         led1 = 1; // LED is ON
+         wait(0.2); // 200 ms
+         led1 = 0; // LED is OFF
+         wait(0.8); // 800 ms
+     }
 
-    btn.fall(queue.event(toggle));
+
+   
 }
